a6846c4f497c263711c8e0482938fe4ee732bce6
Virgínia Arruda <vica_arruda@hotmail.com>
2022-07-27 11:42:26 -0300

    feat: get elements html

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('#btn')
+const input = document.querySelector('#input-cep')
+const subContainerDescription = document.querySelector('.sub-container-description')
+const cep = document.querySelector('#cep')
+const state = document.querySelector('#state')
+const city = document.querySelector('#city')
+const street = document.querySelector('#street')
+
